24c4d11ece65db2d18fdb61adbb2689780e071bc
cacardenasi <ca.cardenasi@duocuc.cl>
2025-12-15 21:56:57 -0300

    Update CentroCultivo.java

--- src/main/java/model/CentroCultivo.java
+++ src/main/java/model/CentroCultivo.java
@@ -1,4 +1,25 @@
-package model;
+package salmontt.model;
 
-public class CentroCultivo {
+public class CentroCultivo implements Registrable {
+
+    private String codigo;
+    private String ubicacion;
+
+    public CentroCultivo(String codigo, String ubicacion) {
+        this.codigo = codigo;
+        this.ubicacion = ubicacion;
+    }
+
+    @Override
+    public void mostrarResumen() {
+        System.out.println("Centro de Cultivo - Código: " + codigo + ", Ubicación: " + ubicacion);
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
 }

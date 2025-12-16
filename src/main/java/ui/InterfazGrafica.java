52f681c0d0fb3cf6e60f8a80ef8c12ed27f99232
cacardenasi <ca.cardenasi@duocuc.cl>
2025-12-15 22:02:00 -0300

    Update InterfazGrafica.java

--- src/main/java/ui/InterfazGrafica.java
+++ src/main/java/ui/InterfazGrafica.java
@@ -1,17 +1,65 @@
-package ui;
+package salmontt.ui;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import salmontt.data.GestorEntidades;
+import salmontt.model.*;
+import javax.swing.JOptionPane;
+
+public class InterfazGrafica {
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        GestorEntidades gestor = new GestorEntidades();
+
+        while (true) {
+            String[] opciones = {
+                    "Agregar Proveedor",
+                    "Agregar Empleado",
+                    "Agregar Centro de Cultivo",
+                    "Agregar Planta de Proceso",
+                    "Mostrar Registros",
+                    "Salir"
+            };
+
+            int opcion = JOptionPane.showOptionDialog(
+                    null,
+                    "Seleccione una opción",
+                    "Sistema Salmontt",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    opciones,
+                    opciones[0]
+            );
+
+            if (opcion == 0) {
+                String id = JOptionPane.showInputDialog("ID del proveedor:");
+                String nombre = JOptionPane.showInputDialog("Nombre del proveedor:");
+                gestor.agregarEntidad(new Proveedor(id, nombre));
+
+            } else if (opcion == 1) {
+                String id = JOptionPane.showInputDialog("ID del empleado:");
+                String nombre = JOptionPane.showInputDialog("Nombre del empleado:");
+                String cargo = JOptionPane.showInputDialog("Cargo del empleado:");
+                gestor.agregarEntidad(new Empleado(id, nombre, cargo));
+
+            } else if (opcion == 2) {
+                String codigo = JOptionPane.showInputDialog("Código del centro:");
+                String ubicacion = JOptionPane.showInputDialog("Ubicación del centro:");
+                gestor.agregarEntidad(new CentroCultivo(codigo, ubicacion));
+
+            } else if (opcion == 3) {
+                String nombre = JOptionPane.showInputDialog("Nombre de la planta:");
+                int capacidad = Integer.parseInt(
+                        JOptionPane.showInputDialog("Capacidad en toneladas:")
+                );
+                gestor.agregarEntidad(new PlantaProceso(nombre, capacidad));
+
+            } else if (opcion == 4) {
+                gestor.mostrarEntidades();
+
+            } else {
+                break;
+            }
         }
     }
 }

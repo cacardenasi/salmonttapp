4b2c366c021411e45880252ea496606eb9cf040d
cacardenasi <ca.cardenasi@duocuc.cl>
2025-12-15 22:00:27 -0300

    Update Proveedor.java

--- src/main/java/model/Proveedor.java
+++ src/main/java/model/Proveedor.java
@@ -1,4 +1,25 @@
-package model;
+package salmontt.model;
 
-public class Proveedor {
+public class Proveedor implements Registrable {
+
+    private String id;
+    private String nombre;
+
+    public Proveedor(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void mostrarResumen() {
+        System.out.println("Proveedor - ID: " + id + ", Nombre: " + nombre);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }

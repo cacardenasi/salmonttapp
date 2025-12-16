778714064505c91543dc99ad09dcf2881cb5d5e1
cacardenasi <ca.cardenasi@duocuc.cl>
2025-12-15 21:59:32 -0300

    Update PlantaProceso.java

--- src/main/java/model/PlantaProceso.java
+++ src/main/java/model/PlantaProceso.java
@@ -1,4 +1,28 @@
-package model;
+package salmontt.model;
 
-public class PlantaProceso {
+public class PlantaProceso implements Registrable {
+
+    private String nombre;
+    private int capacidad;
+
+    public PlantaProceso(String nombre, int capacidad) {
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+    }
+
+    @Override
+    public void mostrarResumen() {
+        System.out.println(
+                "Planta de Proceso - Nombre: " + nombre +
+                        ", Capacidad: " + capacidad + " toneladas"
+        );
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
 }

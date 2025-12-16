4e1ad111b5f14f415d6d91fe80cfd33b1eb67571
cacardenasi <ca.cardenasi@duocuc.cl>
2025-12-15 21:57:50 -0300

    Update Empleado.java

--- src/main/java/model/Empleado.java
+++ src/main/java/model/Empleado.java
@@ -1,4 +1,31 @@
-package model;
+package salmontt.model;
 
-public class Empleado {
+public class Empleado implements Registrable {
+
+    private String id;
+    private String nombre;
+    private String cargo;
+
+    public Empleado(String id, String nombre, String cargo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+    }
+
+    @Override
+    public void mostrarResumen() {
+        System.out.println("Empleado - ID: " + id + ", Nombre: " + nombre + ", Cargo: " + cargo);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
 }
